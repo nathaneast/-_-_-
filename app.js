da4100cfab9ed42561a4a972fdae44bea2af26df
nathan <ahrl_ahrl@naver.com>
2020-07-26 10:26:00 +0900

    group functions into object

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const modalContent = document.querySelector('.modal-content');
 const userList = document.querySelector('.userList');
 
 function init() {
-  const modals = {
+  const currDate = new Date();
+  const modalsProcess = {
     initMarkup: () => {
       const currContent = document.createElement('div');
       const message = document.createElement('div');
@@ -32,169 +33,164 @@ function init() {
       modal.classList.remove('hidden');
       modalOverlay.addEventListener('click', (e) => {
         if (e.target === e.currentTarget) {
-          resetContent();
+          calenderProcess.resetContent();
           modal.classList.add('hidden');
         }
       });
     },
     closeModal: () => {
       modal.classList.add('hidden');
-      resetContent();
+      calenderProcess.resetContent();
     },
     warningMessage: {
       userCount: () => {
-        resetContent();
-        modals.initMarkup();
-        
+        calenderProcess.resetContent();
+        modalsProcess.initMarkup();
+
         const warningMessage = document.createElement('h3');
         const closeBtn = document.createElement('button');
-  
+
         document.querySelector('.message').appendChild(warningMessage);
         document.querySelector('.message').appendChild(warningMessage);
         document.querySelector('.btns').appendChild(closeBtn);
-  
+
         warningMessage.innerText = '인원은 5명을 초과할 수 없습니다';
         closeBtn.innerText = '확인';
-  
-        closeBtn.addEventListener('click', modals.closeModal);
+
+        closeBtn.addEventListener('click', modalsProcess.closeModal);
       },
       userName: () => {
-        resetContent();
-        modals.initMarkup();
-  
+        calenderProcess.resetContent();
+        modalsProcess.initMarkup();
+
         const warningMessage = document.createElement('h3');
         const returnAddUserBtn = document.createElement('button');
         const closeBtn = document.createElement('button');
-  
+
         document.querySelector('.message').appendChild(warningMessage);
         document.querySelector('.btns').appendChild(returnAddUserBtn);
         document.querySelector('.btns').appendChild(closeBtn);
-  
+
         warningMessage.innerText = '이름을 1 ~ 5 자리로 입력 해주세요';
         returnAddUserBtn.innerText = '다시 생성';
         closeBtn.innerText = '닫기';
-  
+
         returnAddUserBtn.addEventListener('click', () => {
-          resetContent();
-          addUser();
+          calenderProcess.resetContent();
+          userProcess.addUser();
         });
         closeBtn.addEventListener('click', () => {
-          resetContent();
-          modals.closeModal();
+          calenderProcess.resetContent();
+          modalsProcess.closeModal();
         });
       }
     }
   }
 
-  let currDate = new Date();
+  const userProcess = {
+    userListUp: () => {
+      const newUser = document.createElement('div');
+      const newUserName = document.createElement('span');
 
-  // 첫번째 자식 div만 지우면 되지만 두번 눌러야 동작해서 while처리 추후 리팩토링
-  function resetContent () {
-    while (modalContent.firstChild) {
-      modalContent.firstChild.remove();
-    }
-  }
+      newUserName.innerText = document.querySelector('.userInputName').value;
+      userList.appendChild(newUser);
+      newUser.appendChild(newUserName);
 
-  function userListUp() {
-    const newUser = document.createElement('div');
-    const newUserName = document.createElement('span');
-
-    newUserName.innerText = document.querySelector('.userInputName').value;
-    userList.appendChild(newUser);
-    newUser.appendChild(newUserName);
-
-    // newUser.addEventListener('click', selectDayOf);
-    modals.closeModal();
-  }
-
-  function addUserCheck() {
-    const userNameLength = document.querySelector('.userInputName').value.length;
-    if (userNameLength === 0 || userNameLength > 5) {
-      modals.warningMessage.userName();
-    } else if (userList.childElementCount === 5) {
-      modals.warningMessage.userCount();
-    } else {
-      userListUp();
-    }
-  }
-
-  function addUser() {
-    modals.openModal();
-    modals.initMarkup();
-
-    const addUserMessage = document.createElement('h3');
-    const userInputName = document.createElement('input');
-    const addBtn = document.createElement('button');
-    const closeBtn = document.createElement('button');
-    userInputName.classList.add('userInputName');
-
-    document.querySelector('.message').appendChild(addUserMessage);
-    document.querySelector('.userInput').appendChild(userInputName);
-    document.querySelector('.btns').appendChild(addBtn);
-    document.querySelector('.btns').appendChild(closeBtn);
-
-    addUserMessage.innerText = '이름을 입력 해주세요';
-    addBtn.innerText = '추가';
-    closeBtn.innerText = '닫기';
-
-    addBtn.addEventListener('click', addUserCheck);
-    closeBtn.addEventListener('click', modals.closeModal);
-  }
-
-  function viewCurrMonth() {
-    currMonth.innerText = `${currDate.getFullYear()} 년 ${currDate.getMonth() + 1}월`
-  }
-
-  function removeCalender() {
-    // div로 날짜들 감싸서 div 하나만 지우게 리팩토링
-    while (dates.firstChild) {
-      dates.firstChild.remove();
-    }
-  }
-
-  function monthHandler() {
-    if (event.target.className === 'nextMonthBtn') {
-      currDate.setMonth(currDate.getMonth() + 1);
-    } else {
-      currDate.setMonth(currDate.getMonth() - 1);
-    }
-    removeCalender();
-    dateProcess();
-  }
-
-  function viewCalender(dateCulData) {
-    const loopNumber = dateCulData.firstDay + dateCulData.lastDate;
-    let viewDate = 1;
-
-    for (let i = 0; i < loopNumber; i++) {
-      const dateEle = document.createElement('div');
-      const dateNum = document.createElement('span');
-
-      dates.appendChild(dateEle);
-      dateEle.appendChild(dateNum);
-      dateEle.classList.add('dateEle');
-
-      if (i >= dateCulData.firstDay) {
-        dateNum.innerText = viewDate++;
+      // newUser.addEventListener('click', selectDayOf);
+      modalsProcess.closeModal();
+    },
+    addUserCheck: () => {
+      const userNameLength = document.querySelector('.userInputName').value.length;
+      if (userNameLength === 0 || userNameLength > 5) {
+        modalsProcess.warningMessage.userName();
+      } else if (userList.childElementCount === 5) {
+        modalsProcess.warningMessage.userCount();
+      } else {
+        userProcess.userListUp();
       }
-    }
+    },
+    addUser: () => {
+      modalsProcess.openModal();
+      modalsProcess.initMarkup();
 
-    viewCurrMonth();
+      const addUserMessage = document.createElement('h3');
+      const userInputName = document.createElement('input');
+      const addBtn = document.createElement('button');
+      const closeBtn = document.createElement('button');
+      userInputName.classList.add('userInputName');
+
+      document.querySelector('.message').appendChild(addUserMessage);
+      document.querySelector('.userInput').appendChild(userInputName);
+      document.querySelector('.btns').appendChild(addBtn);
+      document.querySelector('.btns').appendChild(closeBtn);
+
+      addUserMessage.innerText = '이름을 입력 해주세요';
+      addBtn.innerText = '추가';
+      closeBtn.innerText = '닫기';
+
+      addBtn.addEventListener('click', userProcess.addUserCheck);
+      closeBtn.addEventListener('click', modalsProcess.closeModal);
+    }
   }
 
-  function dateProcess() {
-    currDate.setDate(1);
+  const calenderProcess = {
+    // 첫번째 자식 div만 지우면 되지만 두번 눌러야 동작해서 while처리 추후 리팩토링
+    resetContent: () => {
+      while (modalContent.firstChild) {
+        modalContent.firstChild.remove();
+      }
+    },
+    viewCurrMonth: () => {
+      currMonth.innerText = `${currDate.getFullYear()} 년 ${currDate.getMonth() + 1}월`
+    },
+    removeCalender: () => {
+      // div로 날짜들 감싸서 div 하나만 지우게 리팩토링
+      while (dates.firstChild) {
+        dates.firstChild.remove();
+      }
+    },
+    monthHandler: () => {
+      if (event.target.className === 'nextMonthBtn') {
+        currDate.setMonth(currDate.getMonth() + 1);
+      } else {
+        currDate.setMonth(currDate.getMonth() - 1);
+      }
+      calenderProcess.removeCalender();
+      calenderProcess.dateCulculation();
+    },
+    viewCalender: dateCulData => {
+      const loopNumber = dateCulData.firstDay + dateCulData.lastDate;
+      let viewDate = 1;
 
-    const dateCulData = {
-      firstDay: currDate.getDay(),
-      lastDate: new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0).getDate()
+      for (let i = 0; i < loopNumber; i++) {
+        const dateEle = document.createElement('div');
+        const dateNum = document.createElement('span');
+
+        dates.appendChild(dateEle);
+        dateEle.appendChild(dateNum);
+        dateEle.classList.add('dateEle');
+
+        if (i >= dateCulData.firstDay) {
+          dateNum.innerText = viewDate++;
+        }
+      }
+
+      calenderProcess.viewCurrMonth();
+    },
+    dateCulculation: () => {
+      currDate.setDate(1);
+
+      const dateCulData = {
+        firstDay: currDate.getDay(),
+        lastDate: new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0).getDate()
+      }
+      calenderProcess.viewCalender(dateCulData);
     }
-    viewCalender(dateCulData);
   }
-  dateProcess();
+  calenderProcess.dateCulculation();
 
-  backMonthBtn.addEventListener('click', monthHandler);
-  nextMonthBtn.addEventListener('click', monthHandler);
-  addUserBtn.addEventListener('click', addUser);
+  backMonthBtn.addEventListener('click', calenderProcess.monthHandler);
+  nextMonthBtn.addEventListener('click', calenderProcess.monthHandler);
+  addUserBtn.addEventListener('click', userProcess.addUser);
 }
 init();
